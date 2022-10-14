8d8771305e15ce8eae927883684838e4210fceee
akunts <david@akunts.ru>
2022-10-14 16:45:27 +0400

    task 4

---
+++ Task004/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.
+
+Console.Write("Введите число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+int count = -x;
+
+while (count < x + 1)
+{
+    Console.Write(count + " ");
+    count++;
+}
